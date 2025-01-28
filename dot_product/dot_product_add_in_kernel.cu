c08ccc2e42bd2ccdd9d5260a527448cb9799f21f
Ben Glick <glick@glick.cloud>
2025-01-27 21:20:50 -0800

    clean up

--- dot_product/dot_product_add_in_kernel.cu
+++ dot_product/dot_product_add_in_kernel.cu
@@ -11,10 +11,10 @@
  * In another version of this, we will use atomic stores to add from the GPU too.
  */
 __global__ void vec_mul(int *a, int *b, int *c){
-	int idx = threadIdx.x + blockDim.x * blockIdx.x;
+	int idx = threadIdx.x + (blockDim.x * blockIdx.x);
 	int c_d = a[idx] * b[idx];
 	atomicAdd(c, c_d);
-	printf("Hello from thread (%d, %d, %d) idx %d. We MULed %d and %d to get %d\n", threadIdx.x, threadIdx.y, threadIdx.z, idx, a[threadIdx.x], b[threadIdx.x], c_d);
+	//printf("Hello from thread (%d, %d, %d) block %d idx %d. We MULed %d and %d to get %d\n", threadIdx.x, threadIdx.y, threadIdx.z, blockIdx.x, idx, a[threadIdx.x], b[threadIdx.x], c_d);
 }
 
 
@@ -23,7 +23,9 @@ int main(){
 	int *d_a, *d_b, *d_c;
 	// This should be 4 bytes.
 	int size = sizeof(int);
-	int vec_elems = 16;
+	int n_blocks = 100;
+	int threads_per_block = 256;
+	int vec_elems = n_blocks * threads_per_block;
 
 	a = (int *) malloc(sizeof(int) * vec_elems);
 	b = (int *) malloc(sizeof(int) * vec_elems);
@@ -35,23 +37,25 @@ int main(){
 	cudaMalloc(&d_b, size*vec_elems);
 	cudaMalloc(&d_c, size);
 	
+	int cpu_dot = 0;
 	for (int i =0; i<vec_elems;i++){
 		a[i] = 1+i;
-		b[i] = 2 + i; 
+		b[i] = 2 + i;
+		cpu_dot += a[i] * b[i];
 	}
 
 
 	// H2D copies
 	cudaMemcpy(d_a, a, size*vec_elems, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_b, b, size*vec_elems, cudaMemcpyHostToDevice);
-
-	vec_mul<<<1,16,1>>>(d_a, d_b, d_c);
+	// N Blocks, N threads per block, bytes of shared mem
+	vec_mul<<<n_blocks,threads_per_block,0>>>(d_a, d_b, d_c);
 
 	// D2H Copies
 	cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
 	cudaDeviceSynchronize();
 
-	printf("Dot product: %d\n", *c);
+	printf("Dot product: %d, CPU Dot: %d\n", *c, cpu_dot);
 	cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
 	
 	return 0;
